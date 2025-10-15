0a447186550a402c8174bc107195183405b7a960
Gottcollo <gottcollo@gmail.com>
2025-10-15 11:30:59 +0200

    commit: repairs

--- theme.py
+++ theme.py
@@ -18,3 +18,32 @@ themes = [
         "entry_fg": "#ffffff"
     },
 ]
+
+
+current_theme = {"index": 0}
+
+def get_current_theme():
+    return themes[current_theme["index"]]
+
+def next_theme():
+    current_theme["index"] = (current_theme["index"] + 1) % len(themes)
+    return get_current_theme()
+
+def prev_theme():
+    current_theme["index"] = (current_theme["index"] - 1) % len(themes)
+    return get_current_theme()
+
+
+# Neue Hilfsfunktion für Kontrast
+
+def get_contrast_color(bg_color):
+    """
+    bg_color als Hex-String, gibt 'black' oder 'white' zurück
+    abhängig von Helligkeit des Hintergrunds
+    """
+    bg_color = bg_color.lstrip('#')
+    r = int(bg_color[0:2], 16)
+    g = int(bg_color[2:4], 16)
+    b = int(bg_color[4:6], 16)
+    brightness = (r*299 + g*587 + b*114) / 1000
+    return "black" if brightness > 128 else "white"
